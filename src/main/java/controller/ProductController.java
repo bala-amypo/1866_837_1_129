f25d86d43ae89a2002b6d268ee46ac027a92dd78
bala-amypo <bala@amypo.in>
2025-12-23 04:31:05 +0000

    User commit

--- src/main/java/controller/ProductController.java
+++ src/main/java/controller/ProductController.java
@@ -10,35 +10,9 @@ import java.util.List;
 @RequestMapping("/api/products")
 @Tag(name = "ProductController")
 public class ProductController {
-
     private final ProductServiceImpl productService;
 
     public ProductController(ProductServiceImpl productService) {
         this.productService = productService;
-    }
-
-    @PostMapping
-    public Product createProduct(@RequestBody Product product) {
-        return productService.createProduct(product);
-    }
-
-    @PutMapping("/{id}")
-    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
-        return productService.updateProduct(id, product);
-    }
-
-    @GetMapping("/{id}")
-    public Product getProduct(@PathVariable Long id) {
-        return productService.getProductById(id);
-    }
-
-    @GetMapping
-    public List<Product> listProducts() {
-        return productService.getAllProducts();
-    }
-
-    @PutMapping("/{id}/deactivate")
-    public void deactivateProduct(@PathVariable Long id) {
-        productService.deactivateProduct(id);
     }
 }
