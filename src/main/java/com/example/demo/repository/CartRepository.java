bdd26b8f074c5778d76b88c1a702b7ccbc0c406d
bala-amypo <bala@amypo.in>
2025-12-26 08:36:58 +0000

    User commit

--- src/main/java/com/example/demo/repository/CartRepository.java
+++ src/main/java/com/example/demo/repository/CartRepository.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 
-import com.example.demo.entity.Cart;
+import com.example.demo.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
