2369f7b520be37101d944f6408253ac30cc87719
bala-amypo <bala@amypo.in>
2025-12-26 08:33:59 +0000

    User commit

--- src/main/java/com/example/demo/servlet/HealthServlet.java
+++ src/main/java/com/example/demo/servlet/HealthServlet.java
@@ -9,10 +9,10 @@ import java.io.PrintWriter;
 public class HealthServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setStatus(200); [cite: 11]
-        resp.setContentType("text/plain"); [cite: 12]
-        PrintWriter writer = resp.getWriter(); [cite: 12]
-        writer.write("BUNDLE-OK"); [cite: 12]
-        writer.flush(); [cite: 12]
+        resp.setStatus(200);
+        resp.setContentType("text/plain");
+        PrintWriter writer = resp.getWriter();
+        writer.write("BUNDLE-OK");
+        writer.flush();
     }
 }
